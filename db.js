8d669feefc82ee0d98cd613ae18e2c6ce463ecda
Neha Kumari <32375112+sidv93@users.noreply.github.com>
2019-04-07 00:43:45 +0530

    delete

--- db.js
+++
@@ -1,8 +0,0 @@
-import low from 'lowdb';
-import FileSync from 'lowdb/adapters/FileSync';
-
-const adaptor = new FileSync('./db.json');
-const db = low(adaptor);
-db.defaults({twitter: [], instagram: []}).write();
-
-export default db;
